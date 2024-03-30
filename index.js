a6903dbd802d644b10032deb4ea2911ca07babea
nickshakh <shakhbazyann@gmail.com>
2024-03-30 17:33:52 +0400

    Added routes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
+const contactRoutes = require('./routes/routes')
 const PORT = process.env.PORT || 8181;
 
 const url = `http://localhost:${PORT}`
 
 
-app.get('/', (req,res) => {
-    res.send("Hi guys!!!")
-})
+app.use('/api/contacts',contactRoutes)
 
 app.listen(PORT,() => {
     console.log(`Server is listening on ${url}`)
