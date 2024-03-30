421ec364ab39c535eb28bb51047e598f8625a736
nickshakh <shakhbazyann@gmail.com>
2024-03-30 18:13:13 +0400

    Updated error handler,added constants

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,39 @@
+const {constants} = require('../constants')
+
+
 const errorHandler = (err,req,res,next) => {
     const status = res.status ? res.status : 500;
-    res.json({message : err.message, stackTrace : err.stack})
+
+    switch(status) {
+        case constants.VALIDATION_ERROR : 
+            res.json({title : "Validation Failed",
+            message : err.message,
+        })
+            break;
+        case constants.NOT_FOUND :
+            res.json({title:"Not Found",
+            message:err.message
+        })
+            break;
+        case constants.FORBIDDEN :
+            res.json({title:"Forbidden",
+            message:err.message
+        })
+            break;
+        case constants.UNAUTHORIZED :
+            res.json({title:"Unauthorized",
+            message:err.message
+        })
+            break;
+        case constants.SERVER_ERROR :
+            res.json({title:"Server Error",
+            message:err.message
+        })
+            break;
+            default: 
+        console.log("There are no errors!!")
+            break;
+   }
 }
 
 module.exports = errorHandler
